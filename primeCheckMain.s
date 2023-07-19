1bd0352247bb319d6b22c21080c465068203e773
HN1234567 <117358354+HN1234567@users.noreply.github.com>
2023-07-19 14:16:15 -0400

    Update #1 primeCheckMain.s

--- primeCheckMain.s
+++ primeCheckMain.s
@@ -8,9 +8,9 @@
 # Your program should print out "Number n is prime" if the number is prime, and "Number n is not prime if the number is not prime. 
 # The user should be able to enter a "-1" to end the problem. It should print an error if 0, 1, 2 or any negative number other than -1 are entered.
 
-# r4: number entered      r5: end condition
+# r4: number entered      r5: -1
 # r6: number of elements  r7: current divisor
-# r8: modulo result
+# r8: modulo result       
 
 .text
 .global main
@@ -32,23 +32,35 @@ main:
   LDR r4, =numSpace
   LDR r4, [r4]
 
-  # Initialize Loop
+  # Initialize conditions
   MOV r5, #-1
+  MOV r9, #0
   # Divide n by 2 to find number of elements to check
 
   # Start loop
-  startLoop:
+  startSentinelLoop:
+
+  # Check if end loop condition entered, -1.
   CMP r4, r5
-  BEQ endLoop
+  BNE endSentinelLoop
     
     # Loop block
+    # Find remainder of number and divisor, place in r8
+    startForLoop:
     MOV r8, r4 MOD r7
+    CMP r8, r9
     
-
-
-
-
-  endLoop:
+    #Branch if composite
+    BNE endForLoop
+      LDR r0, =compositeResult
+      BL printf
+      B endSentinelLoop
+      
+    endForLoop:
+    ADD r7, r7, #1
+    B startForLoop
+    
+  endSentinelLoop:
 
   #Return to OS
   LDR lr, [sp, #0]
@@ -57,9 +69,11 @@ main:
   
 .data
 
+  compositeResult: .asciz "\nThe number is composite.\n
   numPrompt: .asciz "\nEnter a number to check primality: \n"
-  numInput: .asciz "%d"
+  numInput: .asciz "%d
   numSpace: .word 0
   resultString: .asciz "%s received an %s.\n"
+  
 
 #END main
